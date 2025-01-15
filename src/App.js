defc38003b5cf0a271b80eb68e2b974f02739ec0
garvbahal <garvbahal75@gmail.com>
2025-01-15 17:43:57 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { apiUrl, filterData } from "./data";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 function App() {
-  return <div className="App"></div>;
+  const [courses, setCourses] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(apiUrl);
+        if (!res.ok) {
+          throw Error("Error is here");
+        }
+        const data = await res.json();
+        //save the data
+
+        setCourses(data.data);
+      } catch (err) {
+        toast.error("Something went wrong");
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <Navbar />
+
+      <Filter filterData={filterData} />
+
+      <Cards courses={courses} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "./Card";
+
+const Cards = ({ courses }) => {
+  let allCourses = [];
+  const getCourses = () => {
+    Object.values(courses).forEach((courseCategory) => {
+      courseCategory.forEach((course) => {
+        allCourses.push(course);
+      });
+      return allCourses;
+    });
+  };
+
+  return (
+    <div>
+      {getCourses().map((course) => {
+        return <Card key={course.id} course={course} />;
+      })}
+    </div>
+  );
+};
+
+export default Cards;
